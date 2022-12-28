ca361048f4669a2d7be819dd918a8ef43b9561b1
harsh317 <mistryhk317@gmail.com>
2022-12-28 13:15:27 +0530

    kt file is ready and work properly

--- app/src/main/java/com/harsh_mistry/diceapp/MainActivity.kt
+++ app/src/main/java/com/harsh_mistry/diceapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.harsh_mistry.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollbutoon : Button =  findViewById(R.id.btn_roll)
+        rollbutoon.setOnClickListener {
+
+            rolldice()
+
+        }
+    }
+    private fun rolldice()
+    {
+
+        val dice =  Dice(6)
+        val diceroll = dice.roll()
+
+        val resulttxt : TextView =  findViewById(R.id.txt)
+        resulttxt.text =  diceroll.toString()
+
+    }
+}
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
